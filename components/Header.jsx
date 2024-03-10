446233987b002d87e6de914ad5c99d892a39bf46
teja1000 <99501668+teja1000@users.noreply.github.com>
2024-03-10 12:59:07 +0530

    Navbar completed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,21 @@
-import logo from "../images/logo.png"
+
 import React from "react";
-import { HiOutlinePhoneOutgoing } from "react-icons/hi";
-import { FaCartShopping } from "react-icons/fa6";
+import SearchBar from "./Searchbar";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
 
 
 
 const Header=()=>{
   return (
-  <div className="container mx-auto flex items-center justify-between  py-1 text-white px-2">
-  <img src={logo} alt="logo" className="w-16 h-16" />
-  <div className="flex items-center">
-    <div className="flex items-center mr-2">
-      <HiOutlinePhoneOutgoing  className="text-[30px] mr-2" />
-      <div>
-        <p>Call to buy</p>
-        <p>9876590876</p>
-      </div>
-    </div>
-    <div className="relative">
-      <FaCartShopping className="text-[24px]" />
-      
-        <span className="bg-blue-700 text-white w-5 h-5 rounded-full absolute -top-4 left-2 text-center leading-5 ">
-          0
-        </span>
-      
-    </div>
+  <>
+  <div className="h-20 min-w-full shadow-md flex justify-between items-center bg-blue-200">
+   <div className="ml-8"><HiOutlineShoppingBag className="text-3xl" /></div>
+   <SearchBar/>
+   <div className="mr-14"><MdOutlineShoppingCart className="text-3xl" /></div>
   </div>
-</div>
+</>
 );
   
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import { useEffect, useState } from "react"
+
 import Header from "../components/Header"
 
 
 
 function App() {
-  const [products,setProducts]=useState([]);
-  const [filters,setFilters]=useState([]);
   
-  useEffect(()=>{
-    const fetchproducts=async()=>{
-      try{
-        const data=await fetch('https://phones-dev.herokuapp.com/api/phones');
-        const products=await data.json();
-        setProducts(products);
-        setFilters(products);
-      }
-      catch(err){
-
-      }
-    }
-    fetchproducts();
-  },[])
-
+ 
   return (
     <>
-    <div className=" bg-lime-300">
+    <div>
     <Header/>
     </div>
     </>
